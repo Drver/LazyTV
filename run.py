67b88d3275f256af37eb09545a648e33520d23dc
liubo <214461193@qq.com>
2018-09-17 17:04:30 +0800

    config

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 from VideoDB import Video
+import subprocess
+import time
 
-video_path_array = ["video1", "video2"]
-pot_player_path = 'path'
+video_path_array = ["D:/Movie/[碟中谍1][英语配音][中英双字]/碟中谍1.mp4", 
+					"D:/Movie/[碟中谍2][英语配音][中英双字]/碟中谍2.mp4", 
+					"D:/Movie/[碟中谍3][英语配音][中英双字]/碟中谍3.mp4", 
+					"D:/Movie/[碟中谍4][英语配音][中英双字]/碟中谍4.mp4", 
+					"D:/Movie/[碟中谍5][英语配音][中英双字]/碟中谍5.mp4", 
+					"D:/Movie/[哈利波特与魔法石][英语配音][中英双字]/哈利波特与魔法石.mp4", 
+					"D:/Movie/[哈利波特与密室][英语配音][中英双字]/哈利波特与密室.mp4", 
+					"D:/Movie/[哈利波特与阿兹卡班的囚徒][英语配音][中英双字]/哈利波特与阿兹卡班的囚徒.mp4", 
+					"D:/Movie/[哈利波特与火焰杯][英语配音][中英双字]/哈利波特与火焰杯.mp4", 
+					"D:/Movie/[哈利波特与凤凰社][英语配音][中英双字]/哈利波特与凤凰社.mp4", 
+					"D:/Movie/[哈利波特与混血王子][英语配音][中英双字]/哈利波特与混血王子.mp4", 
+					"D:/Movie/[哈利波特与死亡圣器(上)][英语配音][中英双字]/哈利波特与死亡圣器(上).mp4", 
+					"D:/Movie/[哈利波特与死亡圣器(下)][英语配音][中英双字]/哈利波特与死亡圣器(下).mp4"]
+pot_player_path = 'C:/PotPlayer/PotPlayerMini64.exe'
 db = Video()
 
 while 0 < 1:
@@ -12,4 +26,5 @@ while 0 < 1:
 
         cmd = pot_player_path + " " + video_path + " " + "/seek=" + start
 
-        print(cmd)
+        subprocess.Popen(cmd)
+        time.sleep(cost)
